2f2fa27b6a2a02320ef362b5d44ae1c36c51f04b
root <root@SolusVM2.(none)>
2015-08-15 00:44:54 +0800

    Added permission to run shell scripts.

--- test-mingw.sh
+++ test-mingw.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 echo ""
-echo ""
 echo "fasterjson_press data/test_big.json 10000"
+echo ""
 time -p ./bin/gcc/mingw/fasterjson_press ./bin/data/test_big.json 10000
 echo ""
-echo ""
 echo "fasterjson_press data/test_big2.json 10000"
+echo ""
 time -p ./bin/gcc/mingw/fasterjson_press ./bin/data/test_big2.json 10000
 echo ""
-echo ""
 
+echo ""
 echo "rapidjson_test data/test_big.json 10000"
 time -p ./bin/gcc/mingw/rapidjson_test ./bin/data/test_big.json 10000
 echo ""
-echo ""
 echo "rapidjson_test data/test_big2.json 10000"
-time -p ./bin/gcc/mingw/rapidjson_test ./bin/data/test_big2.json 10000
 echo ""
+time -p ./bin/gcc/mingw/rapidjson_test ./bin/data/test_big2.json 10000
 echo ""
