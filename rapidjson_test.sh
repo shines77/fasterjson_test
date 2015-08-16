401b43127aae53ecd526fe353178c67819c33424
shines77 <gz_shines@msn.com>
2015-08-16 21:10:06 +0800

    Update rapidjson_test.sh
    Signed-off-by: shines77 <gz_shines@msn.com>

--- rapidjson_test.sh
+++ rapidjson_test.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
-sudo time -p ./rapidjson_test ../../data/test_big.json 10000
-sudo time -p ./rapidjson_test ../../data/test_big2.json 10000
+echo ""
+echo "rapidjson_test data/test_big.json 10000"
+echo ""
+time -p ./bin/gcc/linux/rapidjson_test ./bin/data/test_big.json 10000
+echo ""
+echo "rapidjson_test data/test_big2.json 10000"
+echo ""
+time -p ./bin/gcc/linux/rapidjson_test ./bin/data/test_big2.json 10000
+echo ""
